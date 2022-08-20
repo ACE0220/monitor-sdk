90b3b8d925b7dcc8c6950e96cf435347ad26307f
ACE0220 <895047954@qq.com>
2022-08-20 16:38:49 +0800

    feat: 新增手动上报和自动上报

--- src/core/index.ts
+++ src/core/index.ts
@@ -25,28 +25,51 @@ export default class Tracker {
         }
     }
 
-    public setUserId <T extends DefaultOptions['uuid']>(uuid: T) {
+    public setUserId<T extends DefaultOptions['uuid']>(uuid: T) {
         this.data.uuid = uuid;
     }
 
-    public setExtra  <T extends DefaultOptions['extra']>(extra: T) {
+    public setExtra<T extends DefaultOptions['extra']>(extra: T) {
         this.data.extra = extra;
     }
 
-    private captureEvent <T>(mouseEventList: string[], targetKey: string, data?:T) {
+    private captureEvent<T>(mouseEventList: string[], targetKey: string, data?: T) {
         mouseEventList.forEach(ev => {
             window.addEventListener(ev, () => {
                 console.log('监听到了', ev);
+                this.reportTracker({
+                    ev,
+                    targetKey,
+                    data
+                })
             })
         })
     }
 
     private installTracker() {
-        if(this.data.historyTracker) {
+        if (this.data.historyTracker) {
             this.captureEvent(['pushState', 'replaceState', 'popState'], 'history-pv')
         }
-        if(this.data.hashTracker) {
+        if (this.data.hashTracker) {
             this.captureEvent(['pushState'], 'history-pv')
         }
+    }
+
+    // 手动上报
+    public sendTracker <T>(data: T) {
+        this.reportTracker(data);
+    }
+
+    // 自动上报
+    private reportTracker<T>(data: T) {
+
+        const params = Object.assign(this.data, data, { time: new Date().getTime() });
+
+        let headers = {
+            type: 'application/x-www-form-urlencoded',
+        }
+        let blob = new Blob([JSON.stringify(params)], headers)
+
+        navigator.sendBeacon(this.data.requestUrl, blob);
     }
 }
