3e6e2a6a1723228cb7c57237fe57d4d311ccc66f
ACE0220 <895047954@qq.com>
2022-08-20 11:01:46 +0800

    feat: 环境搭建

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import ts from 'rollup-plugin-typescript2';
+import path from 'path';
+import dts from 'rollup-plugin-dts';
+
+export default [
+    {
+        input: './src/core/index.ts',
+        output: [
+            {
+                file: path.resolve(__dirname, './dist/sdk.esm.js'),
+                format: "es"
+            },
+            {
+                file: path.resolve(__dirname, './dist/sdk.cjs.js'),
+                format: "cjs"
+            },
+            {
+                file: path.resolve(__dirname, './dist/sdk.js'),
+                format: "umd",
+                name: 'tracker'
+            },
+        ],
+        plugins: [
+            ts()
+        ]
+    },
+    {
+        input: './src/core/index.ts',
+        output:
+        {
+            file: path.resolve(__dirname, './dist/index.d.ts'),
+            format: 'es'
+        },
+        plugins: [
+            dts(),
+        ]
+    }
+]
